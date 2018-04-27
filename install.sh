eca053c47a16e6d3cd0f5b81b7632dbf633ebe7d
vinceq-usgs <vinceq@usgs.gov>
2018-04-26 20:52:25 -0700

    Set conda variable when installing conda

--- install.sh
+++ install.sh
@@ -30,6 +30,7 @@ if [ ! "$conda" ] ; then
     echo "Install directory: $HOME/miniconda"
     bash miniconda.sh -f -b -p $HOME/miniconda
     rm -f miniconda.sh
+    conda=`which conda`
 fi
 
 # Source bash startup file
